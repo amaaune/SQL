4069097cb7fa0a641532312fa681e24292230092
aamaury <amaury.aune@ynov.com>
2026-02-24 11:53:02 +0100

    [ADD] ajout

--- commandes.sql
+++ commandes.sql
@@ -1,8 +1,8 @@
 -- Création de la table Restaurants
 
 CREATE TABLE "Restaurants" (
-    "IdRestaurant" INTEGER,
-    "Name" VARCHAR(50),
+    "IdRestaurant" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Name" VARCHAR(50) NOT NULL,
     "Planet" VARCHAR(50),
     "Opening_year" INTEGER
 );
@@ -10,17 +10,18 @@ CREATE TABLE "Restaurants" (
 -- Création de la table employés
 
 CREATE TABLE "Employees" (
-	"IdEmployees" INTEGER,
+	"IdEmployees" INTEGER PRIMARY KEY AUTOINCREMENT,
 	"Firstname" VARCHAR(50),
 	"Lastname" VARCHAR(50),
 	"Role" VARCHAR(50),
-	"IdRestaurant" INTEGER
+	"IdRestaurant" INTEGER,
+	FOREIGN KEY (IdRestaurant) REFERENCES Restaurants(IdRestaurant)
 );
 
 -- Creation de la table des plats
 
 CREATE TABLE "Dishes" (
-	"IdDishes" INTEGER,
+	"IdDishes" INTEGER PRIMARY KEY AUTOINCREMENT,
 	"Name" VARCHAR(50),
 	"Price" INTEGER,
 	"Category" VARCHAR(50)
@@ -29,19 +30,22 @@ CREATE TABLE "Dishes" (
 -- Creation orders
 
 CREATE TABLE "Orders" (
-	"IdOrders" INTEGER,
+	"IdOrders" INTEGER PRIMARY KEY AUTOINCREMENT,
 	"IdRestaurant" INTEGER,
 	"Total_amount" INTEGER,
-	"Customer_name" VARCHAR(50)
+	"Customer_name" VARCHAR(50),
+	FOREIGN KEY (IdRestaurant) REFERENCES Restaurants(IdRestaurant)
 );
 
 -- creation orderitems
 
 CREATE TABLE "OrderItems" (
-	"IdOrderItems" INTEGER,
+	"IdOrderItems" INTEGER PRIMARY KEY AUTOINCREMENT,
 	"IdOrders" INTEGER,
 	"IdDishes" INTEGER,
-	"Quantity" INTEGER
+	"Quantity" INTEGER,
+	FOREIGN KEY (IdOrders) REFERENCES Orders(IdOrders),
+    FOREIGN KEY (IdDishes) REFERENCES Dishes(IdDishes)
 );
 
 -- Corriger nos mefaits 
@@ -58,3 +62,16 @@ ADD COLUMN hire_date datetime
 ALTER TABLE Dishes
 ADD COLUMN is_vegan BOOLEAN NULL
 
+-- Initialiser les lignes 
+
+-- exemple restaurants
+INSERT INTO Restaurants (Name, Planet, Opening_year)
+VALUES ('Le Trou Noir Gourmand', 'Sagittarius A*', 2450);
+
+-- exemple employés
+INSERT INTO Employees (Firstname, Lastname, Role, IdRestaurant)
+VALUES ('X Æ A-12', 'Musk-Junior', 'Chef de Cuisine', 3);
+
+-- exemple dishes 
+INSERT INTO Dishes (Name, Price, Category)
+VALUES ('Burger d''Astéroïde Croquant', 45.50, 'Plat');
